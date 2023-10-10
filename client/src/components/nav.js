59913038f07b79b2dae79927e98cb09736a7b273
Trevin Small <trevincub03@gmail.com>
2023-10-09 22:21:00 -0400

    Project setup

---
+++ client/src/components/nav.js
@@ -0,0 +1,18 @@
+const Nav = (() => {
+
+  return (
+    <section id="nav" className="flex flex-row justify-between items-center p-4">
+      <div className="flex flex-row justify-start items-center">
+        <img src="http://placehold.it/" alt=""/>
+      </div>
+      <div className="w-1/3 flex flex-row justify-end items-center gap-x-1">
+        <span>Title</span>
+        <span>Title</span>
+        <span>Title</span>
+      </div>
+    </section>
+  );
+
+});
+
+export { Nav };
